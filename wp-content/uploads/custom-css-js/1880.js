2f207a8c3946de7d9953669a57011e011c19bc75
ubaidmanzoor987 <ubaidmanzoor987@gmail.com>
2020-11-25 14:14:01 +0500

    news and social with popups without imagebix

---
+++ wp-content/uploads/custom-css-js/1880.js
@@ -0,0 +1,12 @@
+<!-- start Simple Custom CSS and JS -->
+<script type="text/javascript">
+/* Default comment here */ 
+
+function hide_and_show(){
+  var e = document.getElementsByClassName('.development_internally');
+  e.style.visibility = "hidden";
+}
+
+
+</script>
+<!-- end Simple Custom CSS and JS -->
