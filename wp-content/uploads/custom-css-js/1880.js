3b3dace492da78446323584e238bb421e126ddc3
ubaidmanzoor987 <ubaidmanzoor987@gmail.com>
2020-11-27 00:04:10 +0500

    our developemnts with filers

--- wp-content/uploads/custom-css-js/1880.js
+++ wp-content/uploads/custom-css-js/1880.js
@@ -2,9 +2,76 @@
 <script type="text/javascript">
 /* Default comment here */ 
 
-function hide_and_show(){
-  var e = document.getElementsByClassName('development_internally');
-  e.style.visibility = "hidden";
+function setVisibilitytoAll (s, visibility){
+  for (var i=0 ; i<s.length ; i++){
+    s[i].style.display = visibility;
+  }
+}
+function show_all(event){
+  document.getElementsByClassName('list_sel')[0].style.color = "white";
+  document.getElementsByClassName('student_sel')[0].style.color = "#8b734b";
+  document.getElementsByClassName('residential_sel')[0].style.color = "#8b734b";
+  document.getElementsByClassName('map_sel')[0].style.color = "#8b734b";
+  document.getElementsByClassName('other_sel')[0].style.color = "#8b734b";
+  document.getElementsByClassName('hotel_sel')[0].style.color = "#8b734b";
+  setVisibilitytoAll(document.getElementsByClassName('student'),"block");
+  setVisibilitytoAll(document.getElementsByClassName('hotel'),"block");
+  setVisibilitytoAll(document.getElementsByClassName('other'),"block");
+  setVisibilitytoAll(document.getElementsByClassName('residential'),"block");
+  event.preventDefault();
+}
+
+function show_student(event){
+  document.getElementsByClassName('student_sel')[0].style.color = "white";
+  document.getElementsByClassName('list_sel')[0].style.color = "#8b734b";
+  document.getElementsByClassName('residential_sel')[0].style.color = "#8b734b";
+  document.getElementsByClassName('map_sel')[0].style.color = "#8b734b";
+  document.getElementsByClassName('other_sel')[0].style.color = "#8b734b";
+  document.getElementsByClassName('hotel_sel')[0].style.color = "#8b734b";
+  setVisibilitytoAll(document.getElementsByClassName('student'),"block");
+  setVisibilitytoAll(document.getElementsByClassName('hotel'),"none");
+  setVisibilitytoAll(document.getElementsByClassName('other'),"none");
+  setVisibilitytoAll(document.getElementsByClassName('residential'),"none");
+  event.preventDefault();
+}
+function show_hotel(event){
+  document.getElementsByClassName('student_sel')[0].style.color = "#8b734b";
+  document.getElementsByClassName('list_sel')[0].style.color = "#8b734b";
+  document.getElementsByClassName('residential_sel')[0].style.color = "#8b734b";
+  document.getElementsByClassName('map_sel')[0].style.color = "#8b734b";
+  document.getElementsByClassName('other_sel')[0].style.color = "#8b734b";
+  document.getElementsByClassName('hotel_sel')[0].style.color = "white";
+  setVisibilitytoAll(document.getElementsByClassName('student'),"none");
+  setVisibilitytoAll(document.getElementsByClassName('hotel'),"block");
+  setVisibilitytoAll(document.getElementsByClassName('other'),"none");
+  setVisibilitytoAll(document.getElementsByClassName('residential'),"none");
+  event.preventDefault();
+}
+function show_residential(){
+   document.getElementsByClassName('student_sel')[0].style.color = "#8b734b";
+    document.getElementsByClassName('list_sel')[0].style.color = "#8b734b";
+    document.getElementsByClassName('residential_sel')[0].style.color = "white";
+    document.getElementsByClassName('map_sel')[0].style.color = "#8b734b";
+    document.getElementsByClassName('other_sel')[0].style.color = "#8b734b";
+    document.getElementsByClassName('hotel_sel')[0].style.color = "#8b734b";
+   setVisibilitytoAll(document.getElementsByClassName('student'),"none");
+  setVisibilitytoAll(document.getElementsByClassName('hotel'),"none");
+  setVisibilitytoAll(document.getElementsByClassName('other'),"none");
+  setVisibilitytoAll(document.getElementsByClassName('residential'),"block");
+  event.preventDefault();
+}
+function show_other(event){
+  document.getElementsByClassName('student_sel')[0].style.color = "#8b734b";
+  document.getElementsByClassName('list_sel')[0].style.color = "#8b734b";
+  document.getElementsByClassName('residential_sel')[0].style.color = "#8b734b";
+  document.getElementsByClassName('map_sel')[0].style.color = "#8b734b";
+  document.getElementsByClassName('other_sel')[0].style.color = "white";
+  document.getElementsByClassName('hotel_sel')[0].style.color = "#8b734b";
+  setVisibilitytoAll(document.getElementsByClassName('student'),"none");
+  setVisibilitytoAll(document.getElementsByClassName('hotel'),"none");
+  setVisibilitytoAll(document.getElementsByClassName('other'),"block");
+  setVisibilitytoAll(document.getElementsByClassName('residential'),"none");
+  event.preventDefault();
 }
 
 
